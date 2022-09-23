ec1598fcf9e40b0d675f3a279b837bc2dbed4bf9
Abhinav.Sharma <Abhinav.Sharma@XAV-602000267.TI.ads>
2022-09-23 13:21:33 +0530

    hello world

---
+++ PageFactoryAssessment/src/test/java/com/parabank/testcase/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.parabank.testcase;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+  @Test
+  public void helloWorldhome() {
+	  System.out.println("This is hello world class");
+  }
+}
